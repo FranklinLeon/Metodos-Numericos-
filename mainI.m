1da35f1871fc9b67206db41f8f4550495b1e88fe
joar05 <91906193+joar05@users.noreply.github.com>
2021-11-04 20:22:17 -0500

    Add files via upload
    codigo de la matriz inversa

---
+++ mainI.m
@@ -0,0 +1,37 @@
+clear all; clc;
+
+% dadas las siguientes matrices encontrar su matriz inversa
+
+% Ejecicio 1
+
+A = [-1 -5 9;4 -3 -1;-3 14 -5];
+
+try
+disp('Algoritmo matriz inversa') 
+[matriz_inveresa] = Inversa(A)
+disp('-----------------------------------------------------------------------')
+disp('Comprobacion')
+matriz_inveresa=inv(A)
+
+catch err
+  %warning(err.identifier,err.message);
+  fprintf('Error: %s\n',err.message);
+  %err.identifier
+end
+disp(newline);
+disp('*************************************************************************')
+disp(newline);
+% Ejecicio 2
+B = [3 6 -2 9;5 4 5 6;-3 8 2 -3;-4 10 3 9];
+try
+disp('Algoritmo matriz inversa') 
+[matriz_inveresa] = Inversa(B)
+disp('-----------------------------------------------------------------------')
+disp('Comprobacion')
+matriz_inveresa=inv(B)
+
+catch err
+  %warning(err.identifier,err.message);
+  fprintf('Error: %s\n',err.message);
+  %err.identifier
+end
