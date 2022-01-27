176cf3d568faefbf75fd0c53e05fc8d078df3cec
joar05 <johathan.pachecom91@ucuenca.edu.ec>
2022-01-27 15:49:50 -0500

    upload

--- Cap_8_Problemas_de_frontera_en_Ecuaciones_Diferenciales_Ordinarias_(EDO)/M_Diferencias_finitas_Ec_Onda/mainonda.m
+++ Cap_8_Problemas_de_frontera_en_Ecuaciones_Diferenciales_Ordinarias_(EDO)/M_Diferencias_finitas_Ec_Onda/mainonda.m
@@ -2,14 +2,19 @@ clc; clear all; close all;
 
 a = -3; b = 3;
 alpha=6; beta=2;
-
+t=a:0.1:b;
 r = @(t) sin(t+2)+2.*t.^5 -5;
 N = 30;
 
+x=-4:0.2:4;
+y=x;
+[X,Y] = meshgrid(x);
+surf(X,Y,X.*exp(-X.^2 - Y.^2)); hold on;
 try
   [soln,t] = onda(a,b,alpha,beta,r,N);
   figure(1)
   plot(t,soln,'-'); hold on;
+
 catch err
   fprintf('Error: %s\n',err.message);
 end_try_catch
